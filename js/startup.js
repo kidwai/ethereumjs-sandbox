4b246f167c68a17583cbcf9e5fcfc0d0f6f8405c
Ubuntu <ubuntu@ip-172-31-57-212.ec2.internal>
2016-06-06 01:15:17 +0000

    updated startup.js

--- js/startup.js
+++ js/startup.js
@@ -1,19 +1,17 @@
-var web3 = require('web3')
-var fs = require('fs')
-var web3 = new web3(new web3.providers.HttpProvider('http://localhost:8545'))
+var web3 = require('web3');
+var fs = require('fs');
+var cron = require('cron');
 
-// array of accounts
-var acc = web3.eth.accounts
+web3 = new web3(new web3.providers.HttpProvider('http://localhost:8545'))
+web3.eth.defaultAccount = web3.eth.coinbase;
+
+
 
 function checkBalances() {
-	acc.forEach( function(acc) {
+	web3.eth.accounts.forEach( function(acc) {
 		console.log(web3.fromWei(web3.eth.getBalance(acc)))	
-	})
-}
+	})}
 function fs_callback (e, contents) { console.log(contents);}
-function get_source(path) { return fs.readFileSync(path, 'utf8', fs_callback);}
-function solc(source) { return web3.eth.compile.solidity(source);}
-
 function tx_callback(e, contract) {
     if (!e) {
 	if (!contract.address) {
@@ -21,29 +19,40 @@ function tx_callback(e, contract) {
 	} else { 
 	    console.log("Contract mined! Address: " + contract.address);
 	}
-    }
-}
-function contract(compiled) { return web3.eth.contract(compiled.info.abiDefinition); }
-
-/* compiled source intro contract from path 
- * argument specifying contract name, in case
- * multiple contracts in one source file
-*/
+   }}
+function get_source(path) { return fs.readFileSync(path, 'utf8', fs_callback);}
+function solc(source) { return web3.eth.compile.solidity(source);}
+function make_contract(compiled) { return web3.eth.contract(compiled.info.abiDefinition); }
 function build(path, name) {
 	var source = get_source(path);
 	var compiled = solc(source)[name];
 	var code = compiled.code;
-	var c = contract(compiled);
-	return {'contract': c, 'code': code};
-}
-
-
-function deploy (c, ind, password, args) {
-	web3.personal.unlockAccount(acc[ind], password);
+	var con = make_contract(compiled);
+	return {'contract': con, 'code': code};}
+function deploy (c, args) {
 	con = c['contract'];
 	code = c['code'];
 	if (typeof args == "undefined") {
-		return con.new({from:acc[ind], data:code, gas:1000000}, tx_callback);
-	}
-	return con.new(args, {from:acc[ind], data:code, gas:1000000}, tx_callback);
+		return con.new({from:web3.eth.defaultAccount, data:code}, tx_callback);
+	}	
+	return con.new(args, {from:web3.eth.defaultAccount, data:code}, tx_callback);} 
+function periodic_job(period, f) {
+	var cronJob =  new cron.job('*/' + period + ' * * * * *', f);
+	return cronJob;
+}
+
+function make_ticker() { return deploy(build('sol/ticker.sol', 'ticker'));}
+function test_ticker(ticker, period, num_ticks) {
+	var ticker_start = Date.now();
+	var tickerJob = periodic_job(period, function() {
+			var elapsed = Date.now() - ticker_start;
+			var expected = parseInt(elapsed/1000/period*num_ticks);
+			console.log(expected);
+			var i;
+			for(i = 0; i < num_ticks; i++) {
+				ticker.tick();
+			}
+		});
+	tickerJob.start();
+	return tickerJob;
 }
